9c1d9f157fd9e0e907ab60d6f7b1f9178830efe0
krvvko <krvvko@gmail.com>
2020-12-12 02:50:47 -0400

    Finished "about me"

--- pages/AboutMe.php
+++ pages/AboutMe.php
@@ -1,17 +1,25 @@
 <div>
     <h1> ABOUT ME</h1>
 <hr>
-     <p>Hello, my name is Kostya, I am <?php
+     <p>Hello, my name is Kostya, I'm <?php
 
         echo (new DateTime())->diff(new DateTime('2005-09-26 12:35:54'))->y;
 
-        ?> years old, and I am a beginner programmer who wants to learn how to create something useful. For now I don't know how to fill the
-         describtion, so I'm just gonna out some pictures of cats (because I really like cats). </p>
+        ?> years old, and I'm a beginner programmer who wants to learn how to create something useful. I'm going to put some random pictures of cats (because cats are awesome). </p>
 
     <img alt="cat" src="/data/pictures/cat1.jpg">
     <img alt="cat" src="/data/pictures/cat2.jpg">
 
-    <p>That's it for now :)</p>
-
+    <p>
+        From the very childhood I liked computers, and everything connected with them, after some moment,
+        I realized that I want to start creating content, and not just consume it. In the
+        <a href="/MyProjects" style="text-decoration: none" class="transitionblackred">My Projects</a>
+        tab, you can view all my projects and examine my work. But creating isn't my only hobby, I'm
+        also a big fan of cats, kittens, music, movies, and video games such as
+        <a href="https://www.escapefromtarkov.com/" style="text-decoration: none" class="transitionblackred">Escape From Tarkov</a>.
+        <br>
+        <img src="/data/videos/TarkovGif.gif" alt="Tarkov Gif" style="z-index: 10;"/>
+        <br>
+        This GIF is an example of a typical Reshala's boy. </p>
 
 </div>
