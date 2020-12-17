71ab6e202548f0c79282458b77cfc636c2ef236b
krvvko <krvvko@gmail.com>
2020-12-17 09:20:50 -0800

    content added

---
+++ pages/MyProjects.php
@@ -0,0 +1,92 @@
+<style>
+
+     main img {
+
+        height: 200px;
+        width: 400px;
+
+    }
+
+     h4 {
+         color: white;
+         font-size: 23px;
+         position: relative;
+         text-align: center;
+         margin-top: -214px;
+         margin-left: 125px;
+         backdrop-filter: blur(6px);
+         opacity: 0;
+         transition:opacity 0.3s, blur 0.3s;
+         max-width: 346px;
+         text-decoration-width: 500px;
+         text-shadow: -1px 1px 0 #000, 1px 1px 0 #000, 1px -1px 0 #000, -1px -1px 0 #000;
+     }
+     h4:hover {
+         opacity: 1;
+
+
+     }
+    h3 {
+
+        position: relative;
+        text-align: center;
+        margin-top: 50px;
+
+
+    }
+
+     h1, h2 {
+
+         font-family: 'Open Sans Condensed', sans-serif;
+
+     }
+</style>
+
+
+<div>
+    <h1>MY PROJECTS</h1>
+
+    <hr>
+
+
+
+        <img src="/data/pictures/youtube.png">
+        <h4 style="padding: 34px 27px;">I created this
+
+            <a href="https://www.youtube.com/channel/UCse1yKfPXvG6phJXWys-8kw?view_as=subscriber" class="transitionblackred" style="text-shadow: none">channel</a>
+
+            in 2017 and I'm making videos about video games such as
+
+            <a href="https://www.escapefromtarkov.com/" class="transitionblackred" style="text-shadow: none">Escape From Tarkov</a>
+
+            and
+
+            <a href="https://www.ubisoft.com/en-us/game/rainbow-six/siege" class="transitionblackred" style="text-shadow: none">Rainbow Six Siege</a>
+
+            on it.</h4>
+
+
+
+
+
+
+
+        <img src="/data/pictures/fk0849hvg2rt13bpqhjy.jpg">
+        <h4 style="padding: 20px 27px;">GitHub - a resource where developers
+            can share their projects with others,and you can also use it as a
+
+            <a href="https://en.wikipedia.org/wiki/Git" class="transitiongreyred" style="text-shadow: none">Git</a>
+
+            . Of course I have a
+
+            <a href="https://github.com/krvvko" class="transitiongreyred" style="text-shadow: none">page</a>
+
+            on it where I'm uploading all my projects.</h4>
+
+
+
+
+
+    <hr>
+
+</div>
